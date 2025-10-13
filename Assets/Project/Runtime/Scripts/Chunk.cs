e066635c795835f8d2bbd77c1896a6fa40941df5
Rui Costa <rui2117.gomes.costa@gmail.com>
2025-10-13 21:10:32 +0100

    Map Generation - New Prefabs
    Agora os prefabs vem na ordem correta com os modelos do André

--- Assets/Project/Runtime/Scripts/Chunk.cs
+++ Assets/Project/Runtime/Scripts/Chunk.cs
@@ -6,7 +6,7 @@ namespace Project.Runtime.Scripts
     {
         public float Speed = 5f;
         public float DestroyZ = -20f;
-        public float size = 0f;
+        public float offset = 0f;
 
         private void Update()
         {
--- Assets/Project/Runtime/Scripts/SectionGenerator.cs
+++ Assets/Project/Runtime/Scripts/SectionGenerator.cs
@@ -3,27 +3,43 @@ using UnityEngine;
 
 public class SectionGenerator : MonoBehaviour
 {
-    [SerializeField] GameObject[] sectionPrefab;
+    [SerializeField] GameObject[] Area1Size6Prefab;
+    [SerializeField] GameObject[] Area1Size15Prefab;
+
+
+    [SerializeField] GameObject[] Area2Size10Prefab;
+    [SerializeField] GameObject[] Area2Size12Prefab;
+    [SerializeField] GameObject[] Area2Size8Prefab;
+
+
+    [SerializeField] GameObject[] Area3Size4Prefab;
+    [SerializeField] GameObject[] Area3Size3Prefab;
+    [SerializeField] GameObject[] Area3Size2Prefab;
+    [SerializeField] GameObject[] Area3Size6Prefab;
+
+
     [SerializeField] GameObject finalPrefab;
     private int sectionCount = 0;
-    [SerializeField] private int sectionLimit;
+    private int sectionLimit = 13;
 
     public void GenSection()
     {
-        if (sectionCount <= sectionLimit)
+        if (sectionCount < sectionLimit)
         {
-            int index = Random.Range(0, 3);
-
-            print(index);
-
-
-            float chunkSize = sectionPrefab[index].GetComponent<Chunk>().size;
-            Vector3 finalPosition = transform.position + new Vector3(0, 0, chunkSize * 5);
-
-            Instantiate(sectionPrefab[index], finalPosition, Quaternion.identity);
-            sectionCount++;
+            if (sectionCount < 5)
+            {
+                GenArea1();
+            }
+            else if (sectionCount < 8)
+            {
+                GenArea2();
+            }
+            else if (sectionCount < 13)
+            {
+                GenArea3();
+            }
         }
-        else if (sectionCount == sectionLimit + 1)
+        else if (sectionCount == sectionLimit)
         {
             GenFinal();
             sectionCount++;
@@ -31,6 +47,148 @@ public class SectionGenerator : MonoBehaviour
         else
         {
             print("Section limit reached");
+        }
+    }
+
+    private void GenArea1()
+    {
+        if (sectionCount < 4)
+        {
+            int index = Random.Range(0, Area1Size6Prefab.Length);
+
+            print(index);
+
+
+            float chunkSize = Area1Size6Prefab[index].GetComponent<Chunk>().offset;
+            Vector3 finalPosition = transform.position + new Vector3(0, 0, chunkSize * 5);
+
+            Instantiate(Area1Size6Prefab[index], finalPosition, Quaternion.identity);
+            sectionCount++;
+        }
+        else
+        {
+            int index = Random.Range(0, Area1Size15Prefab.Length);
+
+            print(index);
+
+
+            float chunkSize = Area1Size15Prefab[index].GetComponent<Chunk>().offset;
+            Vector3 finalPosition = transform.position + new Vector3(0, 0, chunkSize * 5);
+
+            Instantiate(Area1Size15Prefab[index], finalPosition, Quaternion.identity);
+            sectionCount++;
+        }
+    }
+
+    private void GenArea2()
+    {
+        if (sectionCount == 5)
+        {
+            int index = Random.Range(0, Area2Size10Prefab.Length);
+
+            print(index);
+
+
+            float chunkSize = Area2Size10Prefab[index].GetComponent<Chunk>().offset;
+            Vector3 finalPosition = transform.position + new Vector3(0, 0, chunkSize * 5);
+
+            Instantiate(Area2Size10Prefab[index], finalPosition, Quaternion.identity);
+            sectionCount++;
+        }
+        else if (sectionCount == 6)
+        {
+            int index = Random.Range(0, Area2Size12Prefab.Length);
+
+            print(index);
+
+
+            float chunkSize = Area2Size12Prefab[index].GetComponent<Chunk>().offset;
+            Vector3 finalPosition = transform.position + new Vector3(0, 0, chunkSize * 5);
+
+            Instantiate(Area2Size12Prefab[index], finalPosition, Quaternion.identity);
+            sectionCount++;
+        }
+        else
+        {
+            int index = Random.Range(0, Area2Size8Prefab.Length);
+
+            print(index);
+
+
+            float chunkSize = Area2Size8Prefab[index].GetComponent<Chunk>().offset;
+            Vector3 finalPosition = transform.position + new Vector3(0, 0, chunkSize * 5);
+
+            Instantiate(Area2Size8Prefab[index], finalPosition, Quaternion.identity);
+            sectionCount++;
+        }
+    }
+    
+    private void GenArea3()
+    {
+        if (sectionCount == 8)
+        {
+            int index = Random.Range(0, Area3Size4Prefab.Length);
+
+            print(index);
+
+
+            float chunkSize = Area3Size4Prefab[index].GetComponent<Chunk>().offset;
+            Vector3 finalPosition = transform.position + new Vector3(0, 0, chunkSize * 5);
+
+            Instantiate(Area3Size4Prefab[index], finalPosition, Quaternion.identity);
+            sectionCount++;
+        }
+        else if (sectionCount == 9)
+        {
+            int index = Random.Range(0, Area3Size3Prefab.Length);
+
+            print(index);
+
+
+            float chunkSize = Area3Size3Prefab[index].GetComponent<Chunk>().offset;
+            Vector3 finalPosition = transform.position + new Vector3(0, 0, 0);
+
+            Instantiate(Area3Size3Prefab[index], finalPosition, Quaternion.identity);
+            sectionCount++;
+        }
+        else if (sectionCount == 10)
+        {
+            int index = Random.Range(0, Area3Size2Prefab.Length);
+
+            print(index);
+
+
+            float chunkSize = Area3Size2Prefab[index].GetComponent<Chunk>().offset;
+            Vector3 finalPosition = transform.position + new Vector3(0, 0, chunkSize * 5);
+
+            Instantiate(Area3Size2Prefab[index], finalPosition, Quaternion.identity);
+            sectionCount++;
+        }
+        else if (sectionCount == 11)
+        {
+            int index = Random.Range(0, Area3Size3Prefab.Length);
+
+            print(index);
+
+
+            float chunkSize = Area3Size3Prefab[index].GetComponent<Chunk>().offset;
+            Vector3 finalPosition = transform.position + new Vector3(0, 0, 0);
+
+            Instantiate(Area3Size3Prefab[index], finalPosition, Quaternion.identity);
+            sectionCount++;
+        }
+        else
+        {
+            int index = Random.Range(0, Area3Size6Prefab.Length);
+
+            print(index);
+
+
+            float chunkSize = Area3Size6Prefab[index].GetComponent<Chunk>().offset;
+            Vector3 finalPosition = transform.position + new Vector3(0, 0, chunkSize * 5);
+
+            Instantiate(Area3Size6Prefab[index], finalPosition, Quaternion.identity);
+            sectionCount++;
         }
     }
 
